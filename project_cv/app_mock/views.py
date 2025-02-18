dc5eb122ec09dab4e8cc6ab5b336aeba02274598
fede <fedetrus@gmail.com>
2025-02-18 14:02:25 -0300

    app:mock act

--- project_cv/app_mock/views.py
+++ project_cv/app_mock/views.py
@@ -1,8 +1,9 @@
+# app_mock/views.py
 from django.views.generic import ListView
 from .models import Technology
 
 class TechnologyListView(ListView):
     model = Technology
-    template_name = 'app_mock/main.html'  # Nombre del template
-    context_object_name = 'technologies'  # Cómo se llamará el queryset en tu template
+    template_name = 'app_mock/main.html' 
+    context_object_name = 'technologies' 
 
