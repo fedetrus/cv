a27064c8830e3dc288b35a6c615ed55bff465e4a
fede <fedetrus@gmail.com>
2025-02-18 01:23:56 -0300

    app_mock: se completan todas las secciones

--- project_cv/app_mock/views.py
+++ project_cv/app_mock/views.py
@@ -3,6 +3,6 @@ from .models import Technology
 
 class TechnologyListView(ListView):
     model = Technology
-    template_name = 'app_mock/technology_list.html'  # Nombre del template
+    template_name = 'app_mock/main.html'  # Nombre del template
     context_object_name = 'technologies'  # Cómo se llamará el queryset en tu template
 
