be406a501b39187f306fa569ace1544d9baae764
Fede <fedetrus@gmail.com>
2025-02-21 16:17:40 -0300

    Se arregla el settings y requiremetns con pillow

--- project_cv/project_cv/settings.py
+++ project_cv/project_cv/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-32u4gify(chv9q&wb67t52jh3szx59$_vwbddl-w8f$vat1-k@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['fedetrus.pythonanywhere.com','localhost','127.0.0.1']
+ALLOWED_HOSTS = ['djangofedetrus.pythonanywhere.com','localhost','127.0.0.1']
 
 
 # Application definition
