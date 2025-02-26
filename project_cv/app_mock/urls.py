05f6429823c3a4bbed117899543334bb784776ed
fede <fedetrus@gmail.com>
2025-02-26 00:53:41 -0300

    Se instala pyppeteer==2.0.0 para opder descargar version pdf, modos light y dark, se agrega su view y se agregan en css de logo_tech el media para que imprima tamaños aecuados los logos, y se agrega otro media en el nav para que no lo imprima en el pdf

--- project_cv/app_mock/urls.py
+++ project_cv/app_mock/urls.py
@@ -1,8 +1,9 @@
 # app_mock/urls.py
 from django.urls import path
 from django.views.generic import TemplateView
-from .views import TechnologyListView
+from .views import TechnologyListView, generate_pdf
 
 urlpatterns = [
     path('', TechnologyListView.as_view(), name='main'),
+    path('descargar-pdf/', generate_pdf, name='descargar_pdf'),
 ]
